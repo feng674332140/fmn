01f37bc83741f880a495823270b8b29edd7e2f0f
fmn <674332140@qq.com>
2019-10-23 16:47:40 +0800

    最开始

---
+++ src/main/java/com/fmn/fmn/FmnApplication.java
@@ -0,0 +1,14 @@
+package com.fmn.fmn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FmnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FmnApplication.class, args);
+        System.out.print(1111);
+    }
+
+}
---
+++ src/test/java/com/fmn/fmn/FmnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fmn.fmn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FmnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
